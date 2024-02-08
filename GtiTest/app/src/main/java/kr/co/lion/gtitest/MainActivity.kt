e37ffea5bf1ab69abaa5a69b081e36041750ef85
chanho <uni070@naver.com>
2024-02-08 13:48:56 +0900

    git test

---
+++ GtiTest/app/src/main/java/kr/co/lion/gtitest/MainActivity.kt
@@ -0,0 +1,17 @@
+package kr.co.lion.gtitest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val a1 = 1
+        val a2 = 2
+        val a3 = 3
+        val a4 = 4
+
+    }
+}
